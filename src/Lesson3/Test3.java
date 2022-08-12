d9dcb173c19152833a831bec7c578c88ad20d84e
tukzar07 <tukzar989@gmail.com>
2022-08-12 14:15:13 +0300

    Initial commit

---
+++ src/Lesson3/Test3.java
@@ -0,0 +1,11 @@
+package Lesson3;
+
+public class Test3 {
+    public static void main (String [] args){
+        int a = 5;
+        int b  =a * 2;
+
+        int x=1, y=2, z=3;
+        int x=1, y=2, z=3;
+    }
+}
