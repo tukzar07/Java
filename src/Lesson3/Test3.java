b575b8b5966c5f0a2531084bfaeffa5f8b153cb6
tukzar07 <tukzar989@gmail.com>
2022-08-16 16:40:35 +0300

    add boolean

--- src/Lesson3/Test3.java
+++ src/Lesson3/Test3.java
@@ -2,10 +2,18 @@ package Lesson3;
 
 public class Test3 {
     public static void main (String [] args){
-        int a = 5;
-        int b  =a * 2;
+        int i1 = 5;
+        int i2 = 11;
+        double d1 = 5.5;
+        double d2 = 1.3;
+        long l = 20l;
+        double result = 0;
+        result = i2/d1 + d2 % i1 -l;
 
-        int x=1, y=2, z=3;
-        boolean d = true;
+        System.out.println(result);
+        int a = 5;
+        int b = 8;
+        System.out.println(a-- - --a + ++a + a++ +a);
+        System.out.println(++b - b++ + ++b - --b);
     }
 }
