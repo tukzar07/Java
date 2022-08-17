3054865e0b1d4eea96419b51fb85a08200ab1244
tukzar07 <tukzar989@gmail.com>
2022-08-17 17:07:51 +0300

    add Lesson4

---
+++ src/Lesson4/BankAccount.java
@@ -0,0 +1,13 @@
+package Lesson4;
+
+public class BankAccount {
+    int id = 1;
+    String name;
+    double balance;
+
+    public static void main(String[] args) {
+        BankAccount bA = new BankAccount();
+
+
+    }
+}
---
+++ src/Lesson4/Test10.java
@@ -0,0 +1,14 @@
+package Lesson4;
+
+public class Test10 {
+    public static void main(String[] args) {
+    System.out.println("Hello world!!!");
+    }
+}
+
+class Test11{
+    public static void main(String[] args) {
+        System.out.println("Welcome matherfacker!!!!");
+    }
+
+}
